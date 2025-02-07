efb762ad0bd9856f409791acb4789da121a27363
Andres Haro <hello@aharo.dev>
2025-02-06 20:18:20 -0700

    deleting comments from role assignment

--- IH-EPAC-TF-Testing/modules/RoleAssignment/variables.tf
+++ IH-EPAC-TF-Testing/modules/RoleAssignment/variables.tf
@@ -14,17 +14,3 @@ variable "role_assignments" {
     skip_service_principal_aad_check = optional(bool)
   }))
 }
-# variable "principal_id" {
-#   description = "The principal ID to assign the role to."
-#   type        = string
-# }
-
-# variable "role_name" {
-#   description = "The name of the role to assign."
-#   type        = string
-# }
-
-# variable "scope_id" {
-#   description = "The scope at which the role assignment applies."
-#   type        = string
-# }
